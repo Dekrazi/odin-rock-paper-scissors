c4ac7505851ca4c55e9be8221536f26cc698f56a
Dekrazi <radoslaw.ziajka@gmail.com>
2024-04-21 11:10:23 +0200

    add player choice & main logic

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 const choices = ["rock", "paper", "scissors"]
 
 function getComputerChoice(choices) {
-    return choices[Math.floor(choices.length * Math.random())];
+    const computerChoice = choices[Math.floor(choices.length * Math.random())];
+    console.log("Computer choice:", computerChoice); // Debugging
+    return computerChoice;
 }
 
-computerChoice = getComputerChoice(choices)
+function getPlayerChoice(choices) {
+    let playerChoice
+    do {
+        playerChoice = prompt("rock, paper or scissors?").toLowerCase();
+        if (!choices.includes(playerChoice)) {
+            console.error("Invalid choice.");
+        }
+    } while (!choices.includes(playerChoice) && playerChoice !== null);
+    return playerChoice;
+}
 
+// main loop
+
+function playGame() {
+    const computerChoice = getComputerChoice(choices);
+    const playerChoice = getPlayerChoice(choices);
+
+    if (computerChoice === playerChoice) {
+        console.log("It's a tie!");
+    } else if (
+        (computerChoice === "rock" && playerChoice === "scissors") ||
+        (computerChoice === "paper" && playerChoice === "rock") ||
+        (computerChoice === "scissors" && playerChoice === "paper")
+    ) {
+        console.log("Computer won!");
+    } else {
+        console.log("Player won!");
+    }
+}
+
+playGame();
